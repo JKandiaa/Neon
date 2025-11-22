d63215c5e4711376f31594a49fcd91f6aee5e151
JKandiaa <jkwanguii668@gmail.com>
2025-11-22 13:39:56 +0300

    add neon model to admin panel

--- neon/admin.py
+++ neon/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import neon
 
 # Register your models here.
+@admin.register(neon)
+class NeonAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    
